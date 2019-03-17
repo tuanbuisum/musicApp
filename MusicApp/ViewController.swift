3b80ae213e5cfb0f953ced1358bacdf082fee9c7
Bùi Tuấn <admin@admins-macbook-pro.local>
2019-03-17 20:43:51 +0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Admin on 3/17/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
